f08d99d395c2561fe6fb7146cf542639135d7717
王政 <everywangbug@gmail.com>
2024-12-25 18:04:49 +0800

    feat: container and box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import './App.css'
+
+export const Container: React.FC = () => {
+  return (
+    <div className='container'></div>
+  )
+}
+
+export const Box: React.FC = () => {
+  return (
+    <div className='box'></div>
+  )
+}
+
 
 export const App: React.FC = () => {
   return (
-    <div>App</div>
+    <>
+      <Container />
+      <Box />
+    </>
   )
 }
