16fc3a9da9964ac2789914e46c086b928eb90410
王政 <everywangbug@gmail.com>
2024-12-27 17:55:05 +0800

    feat: drop and drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.css'
+import { useDrag, useDrop } from 'react-dnd'
 
 export const Container: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  
+  const [,drop] = useDrop(() => {
+    return {
+      accept: 'box',
+      drop(item) {
+        console.log(item)
+      }
+    }
+  })
+
+  drop(ref)
+  
   return (
-    <div className='container'></div>
+    <div className='container' ref={ref}></div>
   )
 }
 
 export const Box: React.FC = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  
+  const [, drag] = useDrag({
+    type: 'box',
+    item: {
+      color: 'blue'
+    },
+  })
+
+  drag(ref)
+
   return (
-    <div className='box'></div>
+    <div className='box' ref={ref}></div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-console.log(document.getElementById('root'), 'placeholder')
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <DndProvider backend={HTML5Backend}>
+    <App />
+  </DndProvider>
 );
