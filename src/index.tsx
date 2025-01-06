55c1dc85232b6f56051bdc8ac2b423c30042e9b0
王政 <everywangbug@gmail.com>
2025-01-06 10:15:32 +0800

    feat: import tailwind css

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from './App.tsx';
-import { App1 } from './App1.tsx';
+// import { App1 } from './App1.tsx';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { ToDoList } from './toDoList/ToDoList.tsx';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <DndProvider backend={HTML5Backend}>
-    <App1 />
+    <ToDoList />
   </DndProvider>
 );
